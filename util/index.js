c4aadaa602a3f3e625f56140aa482d8dc020ee46
wangjingfeng <wangjingfeng@haodf.com>
2019-07-25 19:21:25 +0800

    修改文档

--- util/index.js
+++ util/index.js
@@ -2,6 +2,7 @@
  * 事件兼容性方法-混子前端也该会的技能
  */
 var EventUtil = {
+    //添加事件
     addHandler: function(element, type, handler){
         if (element.addEventListener){
             element.addEventListener(type, handler, false);
@@ -11,12 +12,15 @@ var EventUtil = {
             element["on" + type] = handler;
         }
     },
+    //获得事件对象
     getEvent: function(event){
         return event ? event : window.event;
     },
+    //获得源对象
     getTarget: function(event){
         return event.target || event.srcElement;
     },
+    //阻止默认事件
     preventDefault: function(event){
         if (event.preventDefault){
             event.preventDefault();
@@ -24,6 +28,7 @@ var EventUtil = {
             event.returnValue = false;
         }
     },
+    //清除事件绑定
     removeHandler: function(element, type, handler){
         if (element.removeEventListener){
             element.removeEventListener(type, handler, false);
@@ -33,6 +38,7 @@ var EventUtil = {
             element["on" + type] = null;
         }
     },
+    //组织事件冒泡和捕获
     stopPropagation: function(event){
         if (event.stopPropagation){
             event.stopPropagation();
